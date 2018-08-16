02702a9c322d17797c01897cb29652168e39278b
tomer weisman <tomerweisman@gmail.com>
2018-08-16 18:41:28 +0300

    functionality of communicating with ethereum

---
+++ src/main/java/com/olam/node/service/application/entities/Participant.java
@@ -0,0 +1,23 @@
+package com.olam.node.service.application.entities;
+
+public class Participant {
+
+    private String role;
+    private String publickey;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPublickey() {
+        return publickey;
+    }
+
+    public void setPublickey(String publickey) {
+        this.publickey = publickey;
+    }
+}
